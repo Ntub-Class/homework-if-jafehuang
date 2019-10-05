8bc60efa47d98b35798a478f92e1cea6abc85c48
Your Name <you@example.com>
2019-10-06 00:25:39 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 // 請介紹兩個字串方法跟數字方法
+// 字串
+// 1.String.ToUpper  傳回此字串轉換為大寫的版本。
+// 2.String.PadLeft  傳回指定之長度的新字串，
+//   其中目前字串的開頭將以空白和或指定的 Unicode 字元填補。
+// 數字
+// 1.Number.isInteger 用来判断给定的参数是否为整数。
+// 2.Number.isFinite 用來檢測傳入的參數是否是一個有限數。
 
 
 // 錢錢大於一個值，叫乾爹
 let money = 40000;
 let limit = 500000; // 限制值
 
+if (money >= 500000) {
+    console.log('乾爹');
+} else {
+    console.log('你哪位');
+}
